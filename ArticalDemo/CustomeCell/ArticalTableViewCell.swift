1438d70914047d2d74e4be30c3de3b5a985243bb
User <user@Users-MacBook-Pro.local>
2019-05-24 16:19:50 +0530

    push code

---
+++ ArticalDemo/CustomeCell/ArticalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticalTableViewCell.swift
+//  ArticalDemo
+//
+//  Created by User on 24/05/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ArticalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ArticalDemo/Utilities/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  ArticalDemo
+//
+//  Created by User on 24/05/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import Foundation
